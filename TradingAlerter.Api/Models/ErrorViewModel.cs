ada38a60009541c464391202cef14251141e1d0e
Фаридун Бердиев <madysoftware@gmail.com>
2022-12-11 02:50:30 +0500

    created initial project

---
+++ TradingAlerter.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TradingAlerter.Api.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
